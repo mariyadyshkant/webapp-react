5d3adde17cbcb64a98358d3e474b4a9ac3178aa3
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-08-06 14:20:40 +0200

    Add CardMovie component and integrate it into HomePage for movie display

---
+++ src/components/CardMovie.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+export default function CardMovie({ movie }) {
+    const imageUrl = movie.image ? `/public/${movie.image}` : '/public/default-movie.jpg';
+
+    return (
+        <>
+            <div className="card">
+                <img src={imageUrl} alt={movie.title} className="card-img-top" />
+                <div className="card-body">
+                    <h5 className="card-title">{movie.title}</h5>
+                    <p className="card-text">{movie.abstract}</p>
+                    <Link to={`/movies/${movie.id}`} className="btn btn-primary">View Details</Link>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from 'react';
+import CardMovie from '../components/CardMovie';
 
 export default function HomePage() {
+
+    const api_server_url = 'http://localhost:3000/api/movies';
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const response = await fetch(api_server_url);
+            const data = await response.json();
+            setMovies(data);
+        };
+
+        fetchMovies();
+    }, []);
+
     return (
         <>
             <div>
                 <h1>Home Page</h1>
-                {/* Additional content for the home page can be added here */}
+                <div className="container">
+                    <div className="row">
+                        {movies.map((movie, index) => (
+                            <div className="col-md-4" key={movie.id ? movie.id : `movie-${index}`}>
+                                <CardMovie movie={movie} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </>
     );
